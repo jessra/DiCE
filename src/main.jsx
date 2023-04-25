ed076976853eb7a2ea8d616b83ac6685c0db515d
jessra <89423609+jessra@users.noreply.github.com>
2023-04-25 13:28:30 -0400

    Agregado logo y tipografia que se piensa utilizar

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from './App.jsx'
 import './index.css'
 
